3c4b757e43f4191ae27f47ed6bfb93f8b8e2d269
Admin <drojjin@univers.su>
2025-04-07 23:58:47 +0700

    добавлена команда старт

--- moduls/handlers/basic.py
+++ moduls/handlers/basic.py
@@ -6,12 +6,14 @@ from moduls.other.static import main_text, main_photo_path
 from moduls.keyboards.main_keyboard import *
 from aiogram.fsm.context import FSMContext
 
+from moduls.utils.comands import set_commands
 
 basic_handlers = Router(name=__name__)
 
 
 @basic_handlers.message(CommandStart())
 async def command_start_handler(message: Message, bot: Bot, state: FSMContext):
+    await set_commands(bot)
     await state.clear()
     await bot.send_photo(message.from_user.id, photo=FSInputFile(path=main_photo_path), caption=main_text, reply_markup=user_main_keyboard())
 
